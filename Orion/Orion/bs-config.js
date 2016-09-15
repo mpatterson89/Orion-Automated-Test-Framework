39ab92ed4aeb3ab3378aecd93ac5b9ea79de819e
mpatterson89 <mpatterson55863@gmail.com>
2016-09-14 20:56:00 -0500

    Add files via upload

---
+++ Orion/Orion/bs-config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Michael on 4/24/2016.
+ */
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | Please report any issues you encounter:
+ |  https://github.com/shakyShane/browser-sync/issues
+ |
+ | For up-to-date information about the options:
+ |  https://github.com/shakyShane/browser-sync/wiki/Working-with-a-Config-File
+ |
+ */
+module.exports = {
+    /*
+     |--------------------------------------------------------------------------
+     | Ghost Mode
+     |--------------------------------------------------------------------------
+     | https://github.com/shakyShane/browser-sync/wiki/options#wiki-ghostmode
+     */
+    /*ghostMode: false*/
+
+
+
+};
